2bf87df7d369566d9325f41014112f2767cdf927
Ricardo Nunes <ramosdasilvar@gmail.com>
2025-05-16 08:31:37 -0300

    Incluindo produto com valor igual a zero

--- src/test/java/modulos/produtos/ProdutosTest.java
+++ src/test/java/modulos/produtos/ProdutosTest.java
@@ -1,10 +1,13 @@
 package modulos.produtos;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
 
 @DisplayName("Testes Web do modulo de produtos")
 public class ProdutosTest {
@@ -19,6 +22,9 @@ public class ProdutosTest {
         // Vou maximizar a tela
         navegador.manage().window().maximize();
 
+        // Vou definir um tempo de espera padrão de 5 segundos
+        navegador.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
         // Navegar para a pagina da Lojinha Web
         navegador.get("http://165.227.93.41/lojinha-web/v2");
         //Fazer login
@@ -29,12 +35,23 @@ public class ProdutosTest {
         navegador.findElement(By.id("senha")).sendKeys("admin");
 
         navegador.findElement(By.id("btn-entrar")).click();
+
         // Vou para a tela de registro de produto
+        navegador.findElement(By.linkText("ADICIONAR PRODUTO")).click();
 
         // Vou preencher dados do produto e o valor sera igaul a zero
+        navegador.findElement(By.id("produtonome")).sendKeys("MacBook Pro");
+        navegador.findElement(By.name("produtovalor")).sendKeys("000");
+        navegador.findElement(By.id("produtocores")).sendKeys("keys");
 
         // Vou submeter o formulario
+        navegador.findElement(By.cssSelector("button[type='submit']")).click();
 
         // Vou validar que a mensagem de erro foi apresentada
+        String mensagemToast = navegador.findElement(By.cssSelector(".toast.rounded")).getText();
+        Assertions.assertEquals("O valor do produto deve estar entre R$ 0,01 e R$ 7.000,00", mensagemToast);
+
+        // Fechar o navegador
+        navegador.close();
     }
 }
