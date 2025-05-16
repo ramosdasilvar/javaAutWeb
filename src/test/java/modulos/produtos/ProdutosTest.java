b426a293315695cf3f92545caa4d02d8665c6948
Ricardo Nunes <ramosdasilvar@gmail.com>
2025-05-16 07:58:50 -0300

    Abrindo o navegador

--- src/test/java/modulos/produtos/ProdutosTest.java
+++ src/test/java/modulos/produtos/ProdutosTest.java
@@ -1,4 +1,33 @@
 package modulos.produtos;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+@DisplayName("Testes Web do modulo de produtos")
 public class ProdutosTest {
+
+    @Test
+    @DisplayName("Não é permitido registrar produto com valor igual a zero")
+    public void testNaoPermiteRegistroProdutoComValorZerado (){
+        // Abrir o navegador
+        System.setProperty("webdriver.chrome.driver", "C:\\Drivers\\chromedriver-win64\\chromedriver.exe");
+        WebDriver navegador = new ChromeDriver();
+
+        // Vou maximizar a tela
+        navegador.manage().window().maximize();
+
+        // Navegar para a pagina da Lojinha Web
+        navegador.get("http://165.227.93.41/lojinha-web/v2");
+        //Fazer login
+
+        // Vou para a tela de registro de produto
+
+        // Vou preencher dados do produto e o valor sera igaul a zero
+
+        // Vou submeter o formulario
+
+        // Vou validar que a mensagem de erro foi apresentada
+    }
 }
